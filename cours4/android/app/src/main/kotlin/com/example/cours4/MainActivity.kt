feaf2fb22d9f35e4dbd73ea22c67833a4f09c8b2
DeclerusAllen <declerusallen20@gmail.com>
2026-02-12 18:33:18 -0500

    version Beta

--- cours4/android/app/src/main/kotlin/com/example/cours4/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cours4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import "package:flutter/material.dart";
-import 'dart:io';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MainApp());
@@ -14,59 +11,594 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF5271FF),
+        scaffoldBackgroundColor: Colors.white,
+      ),
       home: HomeScreen(),
     );
   }
 }
 
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-  final String imageUrl = "https://www.bigfootdigital.co.uk/wp-content/uploads/2020/07/image-optimisation-scaled.jpg";
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
       appBar: AppBar(
-        title: Text("Taxi"),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/kamyonet.png",
-              width: 320.0,
-              height: 240.0,
+        backgroundColor: Color(0xFF5271FF),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white, size: 28),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MenuScreen()),
+            );
+          },
+        ),
+        title: Text(
+          'EBoutikoo',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 12, top: 8, bottom: 8),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Color(0xFF6B8AFF),
+              borderRadius: BorderRadius.circular(6),
             ),
-            Divider(),
-            Image.network(
-              imageUrl,
-              loadingBuilder: (context, child, progress){
-                return progress == null ? child : CircularProgressIndicator();
-              },
-              width: 320.0,
-              height: 240.0,
-            )
+            child: Center(
+              child: Text(
+                'PEYE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.5,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Catégorie 1
+          Container(
+            margin: EdgeInsets.fromLTRB(12, 12, 12, 6),
+            height: 110,
+            decoration: BoxDecoration(
+              color: Color(0xFF001F3F),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(
+              child: Text(
+                'Kategori',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+          
+          // Catégorie 2
+          Container(
+            margin: EdgeInsets.fromLTRB(12, 6, 12, 6),
+            height: 110,
+            decoration: BoxDecoration(
+              color: Color(0xFF001F3F),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(
+              child: Text(
+                'Kategori',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+          
+          Spacer(),
+          
+          Container(
+            padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => DetailScreen()),
+                      );
+                    },
+                    child: _buildProduitCard(),
+                  ),
+                ),
+                SizedBox(width: 12),
+                Expanded(
+                  child: _buildProduitCard(),
+                ),
+              ],
+            ),
+          ),
+          
+          SizedBox(height: 12),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProduitCard() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              color: Color(0xFF001F3F),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(6)),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Savon',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87,
+                  ),
+                ),
+                SizedBox(height: 6),
+                Text(
+                  'Lorem ipsum Lorem ipsum Lorem ipsum',
+                  style: TextStyle(
+                    fontSize: 11,
+                    color: Colors.grey.shade600,
+                    height: 1.3,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.shopping_cart_outlined, size: 20, color: Colors.black87),
+                    SizedBox(width: 20),
+                    Icon(Icons.favorite_border, size: 20, color: Colors.black87),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+} 
+
+
+
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({super.key});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF5271FF),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white, size: 28),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MenuScreen()),
+            );
+          },
+        ),
+        title: Text(
+          'DETAY',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 12, top: 8, bottom: 8),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Color(0xFF6B8AFF),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Center(
+              child: Text(
+                'PEYE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.5,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Color(0xFF001F3F),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              
+              SizedBox(height: 16),
+              
+              Text(
+                'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
+                style: TextStyle(
+                  fontSize: 13,
+                  height: 1.5,
+                  color: Colors.black87,
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Container(
+        width: 56,
+        height: 56,
+        decoration: BoxDecoration(
+          color: Color(0xFFFFA726),
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 8,
+              offset: Offset(0, 4),
+            ),
           ],
-        )
-      )
+        ),
+        child: Icon(
+          Icons.shopping_cart,
+          color: Colors.white,
+          size: 26,
+        ),
+      ),
+    );
+  }
+}
+
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Color(0xFF5271FF),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white, size: 28),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'EBoutikoo',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 12, top: 8, bottom: 8),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Color(0xFF6B8AFF),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Center(
+              child: Text(
+                'PEYE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.5,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Row(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.65,
+            color: Colors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(20),
+                  color: Color(0xFF5271FF),
+                  child: Text(
+                    'EBoutikoo',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                _buildMenuItem('Konekte'),
+                _buildMenuItem('Lis pwodwi', onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProduitsScreen()),
+                  );
+                }),
+                _buildMenuItem('Dekonekte'),
+              ],
+            ),
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                color: Colors.black.withOpacity(0.3),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenuItem(String titre, {VoidCallback? onTap}) {
+    return InkWell(
+      onTap: onTap ?? () {},
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: Colors.grey.shade300, width: 0.5),
+          ),
+        ),
+        child: Text(
+          titre,
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.black87,
+          ),
+        ),
+      ),
     );
   }
 }
 
 
+class ProduitsScreen extends StatefulWidget {
+  const ProduitsScreen({super.key});
 
-class MyStorage{
-  final _secureStorage = FlutterSecureStorage();
+  @override
+  State<ProduitsScreen> createState() => _ProduitsScreenState();
+}
 
-  AndroidOptions _getAndroidOptions(){
-    return AndroidOptions();
+class _ProduitsScreenState extends State<ProduitsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF5271FF),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white, size: 28),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MenuScreen()),
+            );
+          },
+        ),
+        title: Text(
+          'Lis Pwodwi',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 12, top: 8, bottom: 8),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            decoration: BoxDecoration(
+              color: Color(0xFF6B8AFF),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Center(
+              child: Text(
+                'PEYE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 0.5,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(12),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 0.72,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+        ),
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return _buildProduitGridCard();
+        },
+      ),
+      floatingActionButton: Container(
+        width: 56,
+        height: 56,
+        decoration: BoxDecoration(
+          color: Color(0xFFFFA726),
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 8,
+              offset: Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 26,
+        ),
+      ),
+    );
   }
-  Future<void> saveEmail(String email) async {
-    _secureStorage.write(
-      key: 'email',
-      value: email,
-      aOptions: _getAndroidOptions(),
+
+  Widget _buildProduitGridCard() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 120,
+            decoration: BoxDecoration(
+              color: Color(0xFF001F3F),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(6)),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Savon',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Expanded(
+                    child: Text(
+                      'Lorem ipsum Lorem ipsum Lorem ipsum',
+                      style: TextStyle(
+                        fontSize: 11,
+                        color: Colors.grey.shade600,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.shopping_cart_outlined, size: 20, color: Colors.black87),
+                      SizedBox(width: 20),
+                      Icon(Icons.favorite_border, size: 20, color: Colors.black87),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
